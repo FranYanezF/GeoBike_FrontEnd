8087741f491904d3912c9bcf48b0c9d8afc8ab42
FranYanezF <fran.yanez1989@gmail.com>
2023-06-21 18:16:42 -0400

    frontend para consumir google autentificador

--- src/App.js
+++ src/App.js
@@ -1,67 +1,49 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import "./App.css";
 
 
 function App() {
+	const [user, setUser] = useState(null);
 
-  const [userName,setUserName] = useState('')
-  const [isadmin,setIsAdmin] = useState('')
-  const [password,setPassword] = useState('')
-  const [email,setEmail] = useState('')
-  const [listaUsuarios,setlistaUsuarios] = useState([])
+	const getUser = async () => {
+		try {
+			const url = `http://localhost:3001/api/auth/login/success`;
+			const { data } = await axios.get(url, { withCredentials: true });
+			setUser(data.user._json);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  useEffect(()=>{
-    axios.get("http://localhost:3001/api/usuario").then((response)=>{
-      setlistaUsuarios(response.data)
-    })
-  },[])
+	useEffect(() => {
+		getUser();
+	}, []);
 
-  const enviarDatos = () =>{
-    axios.post('http://localhost:3001/api/usuario/insert',{
-      userName : userName,
-      isadmin : isadmin, 
-      password : password,
-      email : email}).then(()=> {
-        alert("envio de datos exitoso")
-      })
-  }
-
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          <label for="name">Nombre:</label>
-          <input type="text" name="userName" onChange={(e) => {
-            setUserName(e.target.value)
-          }}/>
-        </li>
-        <li>
-          <label for="mail">Correo electrónico:</label>
-          <input type="email" id="mail" name="user_mail" onChange={(e) => {
-            setEmail(e.target.value)
-          }}/>
-        </li>
-        <li>
-         <label for="chkisadmin">Es administrador?</label>
-         <input type="checkbox" id="chkisadmin" value="true" onChange={(e) => {
-            setIsAdmin(e.target.value)
-          }}/>
-        </li>
-        <li>
-          <label for="pwd">Password:</label>
-          <input type="password" id="pwd" name="pwd" onChange={(e) => {
-            setPassword(e.target.value)
-          }}/>
-        </li>
-      </ul>
-      <button type="button" onClick={enviarDatos}>Enviar</button>
-      {listaUsuarios.map((user)=>{
-        return <h2> Nombre usuario : {user.NOMBRE_USUARIO} | email usuario : {user.EMAIL_USUARIO}</h2>
-      })}
-    </div>
-  );
+	return (
+		<div className="container">
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={user ? <Home user={user} /> : <Navigate to="/login" />}
+				/>
+				<Route
+					exact
+					path="/login"
+					element={user ? <Navigate to="/" /> : <Login />}
+				/>
+				<Route
+					path="/signup"
+					element={user ? <Navigate to="/" /> : <Signup />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
